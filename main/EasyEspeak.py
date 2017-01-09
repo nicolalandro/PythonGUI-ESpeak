f193753462fa553c0487adced4a8b9cc1b7083e1
nicolalandro <nicolaxx94@live.it>
2017-01-09 11:32:29 +0100

    Correct Enconding

--- main/EasyEspeak.py
+++ main/EasyEspeak.py
@@ -1,5 +1,8 @@
+import os
+
 from tkinter import *
-#import os  # Serve per la riga di comando
+
+# import os  # Serve per la riga di comando
 
 root = Tk()
 root.title('Simple Espeack')
@@ -13,15 +16,19 @@ textArea1.pack()
 
 
 def callback():
-    import os
     # uno = 'espeak -v mb-it4 "'
     uno = 'espeak -v it "'
     due = textArea1.get("1.0", END)
     tre = '"'
-    os.system(uno + due + tre)
+
+    discorso = uno + due + tre
+    discorso = discorso.encode("utf8")
+
+    os.system(discorso)
 
 
-button1 = Button(root, text="Speack", command=callback)
+button1 = Button(root, text="Speak", command=callback)
 button1.pack()
+
 
 root.mainloop()
